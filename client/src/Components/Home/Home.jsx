5afef60f7bcf7465a32dc9b725741035758bd7b2
Galia Angelova <po6tata_na_galia@abv.bg>
2023-11-18 18:37:59 +0200

    Modify home page

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -7,12 +7,32 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
- 
+  var text = $(".typewriter").text();
+
+  var length = text.length;
+  var timeOut;
+  var character = 0;
+
+  (function typeWriter() {
+    timeOut = setTimeout(function () {
+      character++;
+      var type = text.substring(0, character);
+      $(".typewriter").text(type);
+      typeWriter();
+
+      if (character == length) {
+        clearTimeout(timeOut);
+      }
+    }, 150);
+  })();
   return (
     <>
-      <header className="page-header"></header>
-
-  
+      <div id="wrapper">
+        <header className="page-header"></header>
+        <div className={styles.typewriter}>
+          <h1>Welcome to Baby World! </h1>
+        </div>
+      </div>
     </>
   );
 }
